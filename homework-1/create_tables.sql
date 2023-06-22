2786a5fe5e21cbb053d0a2f0409ec854174d0da5
Renkos Alexander <a.a.renkos@gmail.com>
2023-06-23 00:26:05 +0700

    Homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) NOT NUll,
+	contact_name varchar(100) NOT NUll
+);
+
+CREATE TABLE employees
+(
+	employee_id smallserial PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id smallserial PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id smallserial REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,73 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+# Создание подключения
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='841345'
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            # Внесение данных из файла customers_data.csv в таблицу customers
+            with open('north_data/customers_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                        row
+                    )
+
+finally:
+    conn.close()
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='841345'
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            # Внесение данных из файла employees_data.csv в таблицу employees
+            with open('north_data/employees_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                        row
+                    )
+finally:
+    conn.close()
+
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='841345'
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            # Внесение данных из файла orders_data.csv в таблицу orders
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                        row
+                    )
+finally:
+    conn.close()
